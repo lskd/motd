a3bcc5c52fb252b5e4d03046a44986c4d2323a29
lskd <akonif@gmail.com>
2016-02-11 18:02:44 -0800

    clean up routes, adjusted zen_now link accordingly

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'motd/index'  =>  'motd#index'
-  get 'motd/page1'  =>  'motd#page1'
-  get 'page1'  =>  'motd#page1'
-  get 'page2'  =>  'motd#page2'
-  get 'motd/page2'  =>  'motd#page2'
-  post 'page2'  =>  'motd#page2'
   root 'motd#index'
+  get '/page1'  =>  'motd#page1'
+  get '/page2'  =>  'motd#page2'
+  post '/page2'  =>  'motd#page2'
 
 end
