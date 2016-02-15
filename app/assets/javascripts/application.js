d771f3d7a0820057b5522fd777095c18d95082ef
lskd <akonif@gmail.com>
2016-02-15 02:30:24 -0800

    removing turbolinks from framework

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,11 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
+////// removing below
+// require turbolinks
+////// remove above
+
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
