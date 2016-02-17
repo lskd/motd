96d9f34be55e95794797827c271be72d9a53dfa6
lskd <akonif@gmail.com>
2016-02-16 18:49:40 -0800

    removed ../javascripts/application.js and reference script tag in application.html.erb : compile assest true in production environment

--- app/assets/javascripts/application.js
+++
@@ -1,6 +0,0 @@
-////// removing below
-// require jquery
-// require jquery_ujs
-// require turbolinks
-// require_tree .
-////// remove above
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # changing to true for CSS assets to work
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -76,4 +77,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Add secret Key Base
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
 end
