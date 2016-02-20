69a296498d9601d3741e30fdf250de1f8534eabb
lskd <akonif@gmail.com>
2016-02-19 21:42:54 -0800

    remove sdoc from gemfile

--- Gemfile
+++ Gemfile
@@ -5,17 +5,12 @@ gem 'rails', '4.2.0' # with ruby 2.2.1p85
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 
-# gem 'jbuilder', '~> 2.0'
-# bundle exec rake doc:rails generates the API under doc/api.
-gem 'sdoc', '~> 0.4.0', group: :doc
-
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
 
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
-
 
   # Better Errors :: https://github.com/charliesome/better_errors
   gem 'better_errors', group: :development
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,14 +13,10 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  # config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Raise an error on page load if there are pending migrations.
-  # config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
@@ -38,4 +34,10 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Raise an error on page load if there are pending migrations.
+  # config.active_record.migration_error = :page_load
+
+  # Don't care if the mailer can't send.
+  # config.action_mailer.raise_delivery_errors = false
 end
